d4751d23120330ab2c7dbed1574d6f85e4a239cc
Marcos EMR <marcosemr@marcoss-mbp.home>
2020-09-30 19:26:08 +0100

    added some js into cells

--- app.js
+++ app.js
@@ -7,4 +7,28 @@ const cellDivs = document.querySelectorAll(".game-cell");
 let gameIsLive = true;
 let xIsNext = true;
 
+//Event Handlers/Functions
+const handleReset = (e) => {
+    console.log(e);
+}
 
+const handleCellClick = (e) => {
+    const classList = e.target.classList;
+    const location = classList[1];
+
+    if (xIsNext) {
+        classList.add("x");
+        xIsNext = !xIsNext;
+    } else {
+        classList.add("o");
+        xIsNext = !xIsNext;
+    }
+}
+
+
+//EventListeners
+resetDiv.addEventListener("click", handleReset);
+
+for (const cellDiv of cellDivs) {
+    cellDiv.addEventListener("click", handleCellClick)
+}
