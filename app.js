7145178cf0e4d88efab44fa6c7e57315d6af7292
Marcos EMR <marcosemr@marcoss-mbp.home>
2020-09-30 18:46:22 +0100

    added some initial js

---
+++ app.js
@@ -0,0 +1,10 @@
+//HTML Elements
+const statusDiv = document.querySelector(".status");
+const resetDiv = document.querySelector(".reset");
+const cellDivs = document.querySelectorAll(".game-cell");
+
+//Game Variables
+let gameIsLive = true;
+let xIsNext = true;
+
+
