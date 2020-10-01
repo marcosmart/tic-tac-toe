95a344b89b4cb87e487408178f2710d9d91d3b32
Marcos EMR <marcosemr@marcoss-mbp.home>
2020-10-01 15:52:34 +0100

    reaction of game cells

--- app.js
+++ app.js
@@ -6,6 +6,30 @@ const cellDivs = document.querySelectorAll(".game-cell");
 //Game Variables
 let gameIsLive = true;
 let xIsNext = true;
+let winner = null;
+
+
+//Functions Game Status
+const checkGameStatus = () => {
+    const topLeft = cellDivs[0].classList[2];
+    const topMiddle = cellDivs[1].classList[2];
+    const topRight = cellDivs[2].classList[2];
+    const middleLeft = cellDivs[3].classList[2];
+    const middleMiddle = cellDivs[4].classList[2];
+    const middleRight = cellDivs[5].classList[2];
+    const bottomLeft = cellDivs[6].classList[2];
+    const bottomMiddle = cellDivs[7].classList[2];
+    const bottomRight = cellDivs[8].classList[2];
+
+    //Check winner
+    if (topLeft && topLeft === topMiddle && topLeft === topRight) {
+        gameIsLive = false;
+        winner = topLeft;
+        statusDiv.innerHTML = `${topLeft} is the winner!`
+    }
+}
+
+
 
 //Event Handlers/Functions
 const handleReset = (e) => {
@@ -22,9 +46,12 @@ const handleCellClick = (e) => {
 
     if (xIsNext) {
         classList.add("x");
+        checkGameStatus();
+
         xIsNext = !xIsNext;
     } else {
         classList.add("o");
+        checkGameStatus();
         xIsNext = !xIsNext;
     }
 }
